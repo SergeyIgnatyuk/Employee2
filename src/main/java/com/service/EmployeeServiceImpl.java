8942680544e47aeadcc0c870940b8e4cc6cd8580
S.Ignatyuk <signatyuk89@gmail.com>
2020-11-17 10:14:11 +0300

    Employee Transaction v1.0

--- src/main/java/com/service/EmployeeServiceImpl.java
+++ src/main/java/com/service/EmployeeServiceImpl.java
@@ -4,6 +4,9 @@ import com.model.Employee;
 import com.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,21 +22,25 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<Employee> getEmployees() {
         return employeeRepository.findAll().stream().sorted(((o1, o2) -> (int) (o1.getId() - o2.getId()))).collect(Collectors.toList());
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public Employee getEmployeeById(Long id) {
         return employeeRepository.getOne(id);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public void addEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public Employee editEmployeeById(Long id, int departmentId, String jobTitle) {
         Employee employee = employeeRepository.getOne(id);
         employee.setDepartmentId(departmentId);
@@ -42,6 +49,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public void deleteEmployeeById(Long id) {
         employeeRepository.deleteById(id);
     }
